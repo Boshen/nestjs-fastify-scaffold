ecf60cda724090234d0bdb41cfac6e36d6677b56
Boshen <boshenc@gmail.com>
2020-01-03 15:10:25 +0800

    add husky and lint-staged

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,17 @@ import { NestFastifyApplication } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      trustProxy: true,
+      ignoreTrailingSlash: true,
+      requestIdHeader: 'x-request-id',
+      logger: {
+        prettyPrint: true,
+      },
+    })
+  )
   await app.listen(3000)
 }
 
