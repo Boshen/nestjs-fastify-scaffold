f5d449f2956094330c20292329c39df7358caae6
Boshen <boshenc@gmail.com>
2020-01-04 20:34:59 +0800

    add webpack hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter } from '@nestjs/platform-fastify'
 import { NestFastifyApplication } from '@nestjs/platform-fastify'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import * as helmet from 'helmet'
 
 import { AppModule } from './app.module'
+
+declare const module: any
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -16,7 +20,19 @@ async function bootstrap() {
       },
     })
   )
+
+  const options = new DocumentBuilder().setVersion('1.0.0').build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('/', app, document)
+
+  app.use(helmet()).enableCors()
+
   await app.listen(3000)
+
+  if (module.hot) {
+    module.hot.accept()
+    module.hot.dispose(() => app.close())
+  }
 }
 
 bootstrap()
